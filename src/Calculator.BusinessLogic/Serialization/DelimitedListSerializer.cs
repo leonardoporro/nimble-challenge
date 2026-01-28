ba79e01220bf887acb700ef17f01ad686bc4bb5e
Leonardo Porro <mail@leonardoporro.com.ar>
2026-01-28 01:46:41 -0300

    Support 1 custom delimiter of any length using the format: //[{delimiter}]\n{numbers} o example: //[***]\n11***22***33 will return 66
    o all previous formats should also be supported

--- src/Calculator.BusinessLogic/Serialization/DelimitedListSerializer.cs
+++ src/Calculator.BusinessLogic/Serialization/DelimitedListSerializer.cs
@@ -14,15 +14,26 @@ public class DelimitedListSerializer : IListSerializer
         var delimiters = Delimiters;
         var numbersPart = serializedNumbers;
 
-        // Step 6: custom single-character delimiter
-        if (serializedNumbers.StartsWith("//"))
+        // Step 7: custom delimiter of any length: //[delimiter]\n
+        if (serializedNumbers.StartsWith("//["))
+        {
+            var endIndex = serializedNumbers.IndexOf("]\n", StringComparison.Ordinal);
+            if (endIndex < 0)
+                throw new InvalidOperationException("Invalid custom delimiter format.");
+
+            var customDelimiter =
+                serializedNumbers.Substring(3, endIndex - 3);
+
+            delimiters = new[] { customDelimiter };
+            numbersPart = serializedNumbers.Substring(endIndex + 2);
+        }
+        // Step 6: custom single-character delimiter: //d\n
+        else if (serializedNumbers.StartsWith("//"))
         {
             if (serializedNumbers.Length < 4 || serializedNumbers[3] != '\n')
                 throw new InvalidOperationException("Invalid custom delimiter format.");
 
-            var customDelimiter = serializedNumbers[2].ToString();
-            delimiters = new[] { customDelimiter };
-
+            delimiters = new[] { serializedNumbers[2].ToString() };
             numbersPart = serializedNumbers.Substring(4);
         }
 
---
+++ test/Calculator.BusinessLogic.Tests/Step7_CalculatorServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using Calculator.BusinessLogic.Serialization;
+using Calculator.BusinessLogic.Validations;
+using FluentAssertions;
+
+namespace Calculator.BusinessLogic.Tests;
+
+public sealed class Step7_CalculatorServiceTests
+{
+    CalculatorService CreateService()
+    {
+        var serializer = new DelimitedListSerializer
+        {
+            Delimiters = [",", "\n"]
+        };
+
+        var validators = new IListValidator[]
+        {
+            new NegativeNotAllowedValidator(),
+            new UpperBoundNormalizer()
+        };
+
+        return new CalculatorService(serializer, validators);
+    }
+
+    [Fact]
+    public void Resolve_WithLongCustomDelimiter_ReturnsSum()
+    {
+        var service = CreateService();
+
+        var result = service.Resolve("//[***]\n11***22***33");
+
+        result.Should().Be(66);
+    }
+
+    [Fact]
+    public void Resolve_LongCustomDelimiter_WithInvalidValues_IgnoresThem()
+    {
+        var service = CreateService();
+
+        var result = service.Resolve("//[abc]\n1abcfooabc3");
+
+        result.Should().Be(4);
+    }
+
+    [Fact]
+    public void Resolve_LongCustomDelimiter_DoesNotBreakPreviousSingleCharFormat()
+    {
+        var service = CreateService();
+
+        var result = service.Resolve("//#\n2#5");
+
+        result.Should().Be(7);
+    }
+
+    [Fact]
+    public void Resolve_LongCustomDelimiter_DoesNotBreakDefaultDelimiters()
+    {
+        var service = CreateService();
+
+        var result = service.Resolve("1\n2,3");
+
+        result.Should().Be(6);
+    }
+}
